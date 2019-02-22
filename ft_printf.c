b6ef385cdb8c9edc8983b359096e7ff011dd949e
arnaud kremer <neobahamut@live.fr>
2019-02-22 18:21:55 +0100

    asd

--- ft_printf.c
+++ ft_printf.c
@@ -41,7 +41,7 @@ int			ft_printf(const char *format, ...)
 	ft_putstr_test("Apres le va_start\n");
 	if (ft_create_struct(format, handle) <= 0)
 		return (-1);
-		ft_putstr_test((char)handle->str);
+		ft_putstr_test(handle->str);
 		ft_putstr_test("Apres la struct\n");
 	while (handle->str[handle->index])
 	{
