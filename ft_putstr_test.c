3d832c0617e76c965cf9ac204d753d250d2c6047
arnaud kremer <neobahamut@live.fr>
2019-02-22 18:19:38 +0100

    asd

--- ft_putstr_test.c
+++ ft_putstr_test.c
@@ -5,7 +5,7 @@ void    ft_putchar_test(char c)
     write(1, &c, 1);
 }
 
-void    ft_putstr_test(char *str)
+void    ft_putstr_test(const char *str)
 {
     while (*str)
         ft_putchar_test(*str++);
