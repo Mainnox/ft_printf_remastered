3435672356e6dc68fd6734d3883d62ea66eca39d
Arnaud KREMER <akremer@e3r11p8.42.fr>
2019-03-06 13:11:24 +0100

    Sauvegarde auto

--- ft_set_size.c
+++ ft_set_size.c
@@ -9,10 +9,12 @@ void        ft_set_size(t_printf *handle)
             handle->index++;
             if (handle->str[handle->index] == 'h')
             {
-                handle->extra->size = 1;
+				if (handle->extra->size < 1)
+             	   handle->extra->size = 1;
                 handle->index++;
                 break;
             }
+				if (handle->extra->size < 2)
             handle->extra->size = 2;
             break;
         }
@@ -21,16 +23,20 @@ void        ft_set_size(t_printf *handle)
             handle->index++;
             if (handle->str[handle->index] == 'l')
             {
+				if (handle->extra->size < 4)
                 handle->extra->size = 4;
                 handle->index++;
                 break;
             }
+				if (handle->extra->size < 3)
             handle->extra->size = 3;
             break;
         }
 		if (handle->str[handle->index] == 'j')
+				if (handle->extra->size < 5)
 			handle->extra->size = 5;
 		if (handle->str[handle->index] == 'z')
+				if (handle->extra->size < 7)
 			handle->extra->size = 7;
 		handle->index++;
 		break ;
